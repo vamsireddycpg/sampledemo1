661a65abdb5628877684e48558101d359b08442e
vvemulet <vvemulet@CHNSIPDT0T730>
2019-05-23 15:53:51 +0530

    code written to print messages

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to GIT");
+            Console.WriteLine("you have created a repository git");
+            Console.ReadLine();
         }
     }
 }
